c9bf315cb8624b78bdba391a40f9871005f5ae44
rodrigo.baggio.si@gmail.com <Baggio25>
2023-05-08 16:40:20 -0300

    Spring Security - Aula 05 - Entidade usuário e migration

---
+++ src/main/java/med/voll/api/domain/usuario/Usuario.java
@@ -0,0 +1,24 @@
+package med.voll.api.domain.usuario;
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name = "usuarios")
+@Entity(name = "Usuario")
+@Setter
+@Getter
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String login;
+    private String senha;
+
+}
