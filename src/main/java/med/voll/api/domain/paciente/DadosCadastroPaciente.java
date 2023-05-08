c283c1bdad31dd7d03ddc01855d8d5fa3c47b42c
rodrigo.baggio.si@gmail.com <Baggio25>
2023-05-08 15:55:56 -0300

    Boas práticas na API - Personalizando mensagens de erro

--- src/main/java/med/voll/api/domain/paciente/DadosCadastroPaciente.java
+++ src/main/java/med/voll/api/domain/paciente/DadosCadastroPaciente.java
@@ -8,21 +8,22 @@ import jakarta.validation.constraints.Pattern;
 import med.voll.api.domain.endereco.DadosEndereco;
 
 public record DadosCadastroPaciente(
-        @NotBlank
+
+        @NotBlank(message = "Nome é obrigatório")
         String nome,
 
-        @NotBlank
-        @Email
+        @NotBlank(message = "CRM é obrigatório")
+        @Email(message = "O e-mail está no formato inválido")
         String email,
 
-        @NotBlank
+        @NotBlank(message = "Telefone é obrigatório")
         String telefone,
 
-        @NotBlank
-        @Pattern(regexp = "\\d{3}\\.?\\d{3}\\.?\\d{3}\\-?\\d{2}")
+        @NotBlank(message = "CPF é obrigatório")
+        @Pattern(regexp = "\\d{3}\\.?\\d{3}\\.?\\d{3}\\-?\\d{2}", message = "O CPF está no formato inválido")
         String cpf,
 
-        @NotNull
+        @NotNull(message = "Os dados do endereço são obrigatórios")
         @Valid
         DadosEndereco endereco
 ) { }
