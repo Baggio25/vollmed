7dc6e5f5f77f6ee613fc9e8f39d2961e6eb41d29
rodrigo.baggio.si@gmail.com <Baggio25>
2023-05-08 15:04:49 -0300

    Boas práticas na API - Aula 05 - Tratando erro 400

--- src/main/java/med/voll/api/infra/APIExceptionHandler.java
+++ src/main/java/med/voll/api/infra/APIExceptionHandler.java
@@ -3,6 +3,8 @@ package med.voll.api.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -14,4 +16,15 @@ public class APIExceptionHandler{
         return ResponseEntity.notFound().build();
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity tratarErro400(MethodArgumentNotValidException ex) {
+        var errors = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(errors.stream().map(DadosErroValidacao::new).toList());
+    }
+
+    private record DadosErroValidacao(String campo, String msg) {
+        public DadosErroValidacao(FieldError error) {
+            this(error.getField(), error.getDefaultMessage());
+        }
+    }
 }
