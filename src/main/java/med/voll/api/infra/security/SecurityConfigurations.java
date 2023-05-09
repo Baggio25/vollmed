642d3c07c74430d5423876062a86d289630b37b7
rodrigo.baggio.si@gmail.com <Baggio25>
2023-05-09 08:54:11 -0300

    Spring Security - Aula 11 - Controller de autenticacao

--- src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
+++ src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
@@ -2,9 +2,13 @@ package med.voll.api.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
@@ -29,6 +33,22 @@ public class SecurityConfigurations {
                     .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .build();
+    }
+
+    /*
+    * Serve para criar o objeto AuthenticationManager que será utilizado no AutenticacaoController
+    */
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    /*
+    * Configura o algoritmo de hash de senha BCrypt
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
